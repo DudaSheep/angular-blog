6c554064f0ce5be4a84dc640dffd518946c80453
Maria Eduarda <meduarda.carneiro@hotmail.com>
2025-03-23 21:30:05 -0300

    components and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuTitleComponent } from "./components/menu-title/menu-title.component";
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from "./pages/home/home.component";
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuTitleComponent, CommonModule, HomeComponent, SmallCardComponent, BigCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MenuTitleComponent } from '../../components/menu-title/menu-title.component';
+import { BigCardComponent } from "../../components/big-card/big-card.component";
+import { SmallCardComponent } from "../../components/small-card/small-card.component";
+import { MenuBarComponent } from "../../components/menu-bar/menu-bar.component";
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, MenuTitleComponent, BigCardComponent, SmallCardComponent, MenuBarComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
